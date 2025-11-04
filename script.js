601247615db030077a70c8e10dda1bab9b48ffcf
elijahvr <146490334+elijah948534790857@users.noreply.github.com>
2025-11-04 16:44:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 const games = [
-  { name: "Slope", url: "https://slope-game.github.io/" },
-  { name: "Flappy Bird", url: "https://flappybird.io/" },
-  { name: "Run 3", url: "https://run3.io/" },
-  { name: "Drift Boss", url: "https://poki.com/en/g/drift-boss" },
-  { name: "2048", url: "https://play2048.co/" },
-  { name: "Stickman Hook", url: "https://poki.com/en/g/stickman-hook" },
-  { name: "Retro Bowl", url: "https://poki.com/en/g/retro-bowl" },
-  { name: "Temple Run 2", url: "https://poki.com/en/g/temple-run-2" },
-  { name: "Basketball Stars", url: "https://poki.com/en/g/basketball-stars" },
-  { name: "Subway Surfers", url: "https://poki.com/en/g/subway-surfers" },
-  { name: "Vex 7", url: "https://poki.com/en/g/vex-7" },
-  { name: "Moto X3M", url: "https://poki.com/en/g/moto-x3m" },
-  { name: "Cookie Clicker", url: "https://orteil.dashnet.org/cookieclicker/" },
-  { name: "BitLife", url: "https://bitlifeonline.github.io/" },
-  { name: "Getting Over It", url: "https://scratch.mit.edu/projects/298127192/" },
-  { name: "Minecraft Classic", url: "https://classic.minecraft.net/" },
-  { name: "Doodle Jump", url: "https://scratch.mit.edu/projects/447037982/" },
-  { name: "Agar.io", url: "https://agar.io/" },
-  { name: "Krunker", url: "https://krunker.io/" },
-  { name: "Paper.io 2", url: "https://paper-io.com/" },
-  { name: "Tetris", url: "https://tetris.com/play-tetris" },
-  { name: "Among Us Online", url: "https://amongusplay.online/" },
-  { name: "Shell Shockers", url: "https://shellshock.io/" },
-  { name: "Just Fall LOL", url: "https://justfall.lol/" },
-  { name: "FNAF Web", url: "https://scratch.mit.edu/projects/530348311/" },
-  { name: "Helix Jump", url: "https://poki.com/en/g/helix-jump" },
-  { name: "Slope Tunnel", url: "https://slopegame.online/slope-tunnel" },
-  { name: "Runaway Toad", url: "https://poki.com/en/g/runaway-toad" },
-  { name: "Stick Merge", url: "https://poki.com/en/g/stick-merge" },
-  { name: "Dino Game", url: "https://elgoog.im/t-rex/" },
+  { name: "Slope", url: "https://example.com/slope" },
+  { name: "Flappy Bird", url: "https://example.com/flappybird" },
+  { name: "Drift Boss", url: "https://example.com/driftboss" },
+  { name: "2048", url: "https://example.com/2048" },
+  { name: "Run 3", url: "https://example.com/run3" },
+  { name: "Tetris", url: "https://example.com/tetris" },
+  { name: "Stack", url: "https://example.com/stack" },
+  { name: "Basketball Stars", url: "https://example.com/basketballstars" },
+  { name: "Moto X3M", url: "https://example.com/motox3m" },
+  { name: "Subway Surfers", url: "https://example.com/subwaysurfers" },
+  { name: "Temple Run", url: "https://example.com/templerun" },
+  { name: "Pac-Man", url: "https://example.com/pacman" },
+  { name: "Stickman Hook", url: "https://example.com/stickmanhook" },
+  { name: "Jetpack Joyride", url: "https://example.com/jetpackjoyride" },
+  { name: "Happy Wheels", url: "https://example.com/happywheels" },
+  { name: "Crossy Road", url: "https://example.com/crossyroad" },
+  { name: "Super Mario", url: "https://example.com/supermario" },
+  { name: "Helix Jump", url: "https://example.com/helixjump" },
+  { name: "Runaway Miner", url: "https://example.com/runawayminer" },
+  { name: "Geometry Dash", url: "https://example.com/geometrydash" }
 ];
 
-const gameContainer = document.getElementById("games");
+const gameList = document.getElementById("gameList");
+const gameFrame = document.getElementById("gameFrame");
+const backButton = document.getElementById("backButton");
+
 games.forEach(game => {
-  const div = document.createElement("div");
-  div.className = "game";
-  div.innerHTML = `<h3>${game.name}</h3><a href="${game.url}" target="_blank">Play Now</a>`;
-  gameContainer.appendChild(div);
+  const tile = document.createElement("div");
+  tile.className = "game-tile";
+  tile.innerText = game.name;
+  tile.addEventListener("click", () => openGame(game.url));
+  gameList.appendChild(tile);
+});
+
+function openGame(url) {
+  gameList.classList.add("hidden");
+  gameFrame.src = url;
+  gameFrame.classList.remove("hidden");
+  backButton.classList.remove("hidden");
+}
+
+backButton.addEventListener("click", () => {
+  gameFrame.classList.add("hidden");
+  gameFrame.src = "";
+  backButton.classList.add("hidden");
+  gameList.classList.remove("hidden");
 });
